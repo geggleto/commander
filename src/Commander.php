dce09ea1069c46b205ca37032207c49b7235da88
Glenn Eggeton <geggleton@mobials.com>
2016-12-02 13:01:34 -0500

    Coupling to Slim for Concept

--- src/Commander.php
+++ src/Commander.php
@@ -12,6 +12,9 @@ namespace Commander;
 use Interop\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+use Slim\Route;
 
 /**
  * Class Commander
@@ -26,14 +29,18 @@ class Commander
     /** @var ContainerInterface  */
     protected $container;
 
+    /** @var \Slim\App  */
+    protected $app;
+
     /**
      * Commander constructor.
-     * @param ContainerInterface $container
+     * @param \Slim\App $app
      */
-    public function __construct(ContainerInterface $container)
+    public function __construct(\Slim\App $app)
     {
         $this->list = [];
-        $this->container = $container;
+        $this->container = $app->getContainer();
+        $this->app = $app;
     }
 
     /**
@@ -98,14 +105,40 @@ class Commander
         return true;
     }
 
-    public function mapCommand($key = '') {
-        $that = $this;
-        return function (ServerRequestInterface $request, ResponseInterface $response, array $args = []) use ($that, $key) {
-            if ($that->handle(new Command($key, array_merge($request->getParsedBody(), $args)))) {
-                return $response->getBody()->write("Success");
-            } else {
-                return $response->getBody()->write("Failed");
-            }
-        };
+    /**
+     * @param array $verbs
+     * @param string $pattern
+     * @param string $commandKey
+     * @param string $commandClass
+     *
+     * @return \Slim\Interfaces\RouteInterface
+     */
+    public function map(array $verbs, $pattern = '', $commandKey = '', $commandClass = '') {
+        $route = $this->app->map($verbs, $pattern, [$this, "__invoke"]);
+        $route->setArgument('commandKey', $commandKey);
+        $route->setArgument('commandClass', $commandClass);
+
+        return $route;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param array $args
+     *
+     * @return Response
+     */
+    public function __invoke(Request $request, Response $response, array $args = [])
+    {
+        /** @var $route Route */
+        $route = $request->getAttribute('route');
+        $commandKey = $route->getArgument('commandKey');
+        $commandClass = $route->getArgument('commandClass');
+
+        $command = new $commandClass($commandKey, array_merge($request->getParsedBody(), $args));
+
+        $handlerResponse = $this->handle($command);
+
+        return $response->write((string)$handlerResponse);
     }
 }
